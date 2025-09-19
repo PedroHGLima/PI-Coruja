2ab8b71e2d6f6d8c9b7a9b24bebb7c8a0efbbf0d
PedroHGLima <pedrohglima01@gmail.com>
2025-09-18 21:48:43 -0300

    Adicionando script para treinamento em cluster

---
+++ model/scripts/run_train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --job-name=coruja_train
+#SBATCH --partition=gpu
+#SBATCH --output=%x_%j.out
+
+
+img_path=~/imgs/pi-coruja-model_latest.sif
+singularity exec --nv $img_path bash -c "
+cd ../src && python3 trainar_cnn.py --data-dir ../data/dataset_train --early-stop-patience 50
+"
