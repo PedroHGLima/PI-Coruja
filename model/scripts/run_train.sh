c1f79560a12e14c81fd35883b2919bf3917e2e8c
PedroHGLima <pedrohglima01@gmail.com>
2025-09-24 22:59:49 -0300

    Treinamento de modelo robusto para midterm

--- model/scripts/run_train.sh
+++ model/scripts/run_train.sh
@@ -5,7 +5,7 @@
 #SBATCH --output=%x_%j.out
 
 
-img_path=~/imgs/pi-coruja-model_latest.sif
+img_path=~/imgs/coruja.sif
 singularity exec --nv $img_path bash -c "
-cd ../src && python3 treinar_cnn.py --data-dir ../data/dataset_train --early-stop-patience 50
+cd ../src && python3 treinar_cnn.py --data-dir ../data/dataset_train --early-stop-patience=20 --unfreeze-head --run-name=v61
 "
