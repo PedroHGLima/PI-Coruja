f7d435cee59f87c1eafbd64518bf6339d05e7ad0
PedroHGLima <pedrohglima01@gmail.com>
2025-09-24 22:59:01 -0300

    Fixando o tamanho das imagens de validação

--- model/src/models.py
+++ model/src/models.py
@@ -28,8 +28,7 @@ transforms_map = {
                              [0.229, 0.224, 0.225])
     ]),
     'val': transforms.Compose([
-        transforms.Resize(512),
-        # transforms.CenterCrop(512),
+        transforms.Resize((512, 512)),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406],
                              [0.229, 0.224, 0.225])
